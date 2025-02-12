1e2c93837598bb7ffc6e130c17d769d20c93adc0
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2025-02-11 22:54:40 -0300

    Añade clase de alineacion temporal.
    La misma incluye las siguiente funcionalidades por el momento
    
    alineacion temporal manual

---
+++ dataframe_operations/temp_align.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+class tempAlign:
+
+    def set_manual_align(self, imu_name, pd_dataframe, offset):
+        pd_dataframe['time_' + imu_name] = pd_dataframe['time_' + imu_name] + offset
+        return pd_dataframe
