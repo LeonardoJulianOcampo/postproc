4e9dd3936a196453ecc65eacfb3abec811967863
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2025-02-12 03:54:37 -0300

    Devuelve solo la columna del dataframe modificado y no el df completo

--- dataframe_operations/temp_align.py
+++ dataframe_operations/temp_align.py
@@ -5,4 +5,4 @@ class tempAlign:
 
     def set_manual_align(self, imu_name, pd_dataframe, offset):
         pd_dataframe['time_' + imu_name] = pd_dataframe['time_' + imu_name] + offset
-        return pd_dataframe
+        return pd_dataframe['time_' + imu_name]
