49248142ab8a43a65cba3baa5db53ff02eafe691
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2025-03-11 00:29:47 -0300

    Agrega opción de transformar a grados o a radianes

--- dataframe_operations/utils.py
+++ dataframe_operations/utils.py
@@ -1,11 +1,17 @@
 import numpy as np
 
 
-def unwrap_columns(dataframe, column_names):
-    for column in column_names:
-        dataframe[column] = np.deg2rad(dataframe[column].to_numpy())
-        dataframe[column] = np.unwrap(dataframe[column].to_numpy())
-        dataframe[column] = np.rad2deg(dataframe[column].to_numpy())
+def unwrap_columns(dataframe, column_names, ang_unit='degree'):
+    if ang_unit == 'degree':
+        for column in column_names:
+            dataframe[column] = np.deg2rad(dataframe[column].to_numpy())
+            dataframe[column] = np.unwrap(dataframe[column].to_numpy())
+            dataframe[column] = np.rad2deg(dataframe[column].to_numpy())
+
+    if ang_unit == 'radians':
+        for column in column_names:
+            dataframe[column] = np.unwrap(dataframe[column].to_numpy())
+            dataframe[column] = np.rad2deg(dataframe[column].to_numpy())
 
     return dataframe
 
