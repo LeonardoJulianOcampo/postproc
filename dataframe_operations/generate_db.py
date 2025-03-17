6d7f2bc9a87252abbc4b8b92a2de55e075d85d49
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2025-03-17 14:29:28 -0300

    Cambios en save_range para mejoras de rendimiento

--- dataframe_operations/generate_db.py
+++ dataframe_operations/generate_db.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+from functools import reduce
 
 class DB:
 
@@ -70,18 +71,75 @@ class DB:
     def save_as_csv(self):
         self.db.to_csv(self._output_dir + 'db.csv')
 
+#    def save_range(self, data_dict):
+#        min_time = data_dict['time_range'][0]
+#        max_time = data_dict['time_range'][1]
+#        columns_to_drop = []
+#
+#        # db_cropped = self.db[(self.db['time_imar'] >= min_time) &
+#        #                     (self.db['time_imar'] <= max_time)]
+#
+#        idx_inf = self.db[self.db['time_imar'] == min_time].index[0]
+#        idx_sup = self.db[self.db['time_imar'] == max_time].index[0]
+#
+#        db_cropped = self.db.loc[idx_inf:idx_sup]
+#
+#        for column_name in db_cropped.columns:
+#            if not column_name.startswith(('time', 'roll', 'yaw', 'pitch')):
+#                columns_to_drop.append(column_name)
+#
+#        db_cropped = db_cropped.drop(columns_to_drop, axis=1)
+#        print(f'guardando en {self._output_dir}{data_dict["name"]}.csv')
+#        db_cropped.to_csv(self._output_dir + data_dict['name'] + '.csv')
+
     def save_range(self, data_dict):
         min_time = data_dict['time_range'][0]
         max_time = data_dict['time_range'][1]
         columns_to_drop = []
+        imu_list = []
+        imu_dbs = []
 
-        db_cropped = self.db[(self.db['time_imar'] >= min_time) &
-                             (self.db['time_imar'] <= max_time)]
+        db = self.db.copy()
 
-        for column_name in db_cropped.columns:
+        for column_name in db.columns:
             if not column_name.startswith(('time', 'roll', 'yaw', 'pitch')):
                 columns_to_drop.append(column_name)
 
-        db_cropped = db_cropped.drop(columns_to_drop, axis=1)
-        print(f'guardando en {self._output_dir}{data_dict["name"]}.csv')
-        db_cropped.to_csv(self._output_dir + data_dict['name'] + '.csv')
+        db_cropped = self.db.drop(columns_to_drop, axis=1)
+
+        for column_name in db_cropped.columns:
+            imu_name = column_name.split('_')[1]
+            if imu_name not in imu_list:
+                imu_list.append(imu_name)
+
+        print(f'lista de imus detectadas: {imu_list}')
+        print(f'db_cropped.columns: {db_cropped.columns}')
+
+        for imu in imu_list:
+            new_df = pd.DataFrame()
+            for column_name in db_cropped.columns:
+                if column_name.endswith(imu):
+                    new_df[column_name] = db_cropped[column_name]
+                    if column_name.startswith('time_'):
+                        new_df.rename(inplace=True, columns={"time_" + imu: 'time'})
+                        print(f'renombrando columna time_{imu} por time')
+                        new_df = new_df.dropna(how="all")
+            imu_dbs.append(new_df)
+            print('df agregado a imu_dbs')
+
+        imu_dbs[0] = pd.merge_ordered(imu_dbs[0], imu_dbs[1], on='time', how='outer')
+        imu_dbs[2].to_csv(self._output_dir + 'tel_data' + '.csv')
+        imu_dbs[0] = pd.merge_ordered(imu_dbs[0], imu_dbs[2], on='time', how='outer')
+
+        merged_df = imu_dbs[0].copy()
+
+        for column in merged_df.columns:
+            if not column.startswith("time"):
+                merged_df[column] = merged_df[column].interpolate(method='linear')
+                print(f'column: {column} interpolated')
+
+        df_cropped = merged_df[(merged_df['time'] >= min_time) &
+                               (merged_df['time'] <= max_time)]
+
+        df_cropped.to_csv(self._output_dir + data_dict['name'] + '.csv')
+        return df_cropped
