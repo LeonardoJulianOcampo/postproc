e468eb3891f905cf5b9f69f4f43fdef2edeac3c5
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2025-03-17 14:30:38 -0300

    Comienza metodo para quitar discontinuidades en set de datos
    Todavía no terminado

--- dataframe_operations/utils.py
+++ dataframe_operations/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def unwrap_columns(dataframe, column_names, ang_unit='degree'):
@@ -21,3 +22,22 @@ def align_samples(dataframe):
     for column in dataframe.columns:
         if column.startwith('time'):
             pass
+
+
+def fix_discontinuities(pandas_df, threshold):
+    pandas_df = pandas_df.drop(columns=['Latitude_xsens', 'Longitude_xsens'])
+
+    diff_columns = {column + '_diff': pandas_df[column].diff()
+                    for column in pandas_df.columns if not column.startswith('time')}
+
+    work_df = pd.concat([pandas_df, pd.DataFrame(diff_columns)], axis=1)
+
+    for column in work_df.columns:
+        if column.endswith('_diff'):
+            base_col = column.rsplit('_', 1)[0]
+            mask = (work_df[column] > 0) & (work_df[column] > abs(threshold))
+            work_df.loc[mask, base_col] -= work_df.loc[mask, column]
+
+    work_df = work_df.drop(columns=[col for col in work_df.columns if col.endswith('_diff')])
+
+    return work_df
